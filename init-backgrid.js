ba4d022b9a7b124c5b8b9635ba9b4d652cca6b7d
Wes Jones <waj194@gmail.com>
2015-06-22 11:55:45 -0400

    Create custom DrupalHtmlCell and DrupalRow.

--- init-backgrid.js
+++ init-backgrid.js
@@ -5,53 +5,80 @@
 
 (function ($) {
 
-Drupal.behaviors.backgrid = {
-  attach: function (context, settings) {
+  // Extend cell to include an "html" data type.
+  Backgrid.DrupalHtmlCell = Backgrid.Cell.extend({
+    className: "drupal-html-cell",
+    render: function () {
+      this.$el.empty();
+      var rawValue = this.model.get(this.column.get("name"));
+      // var formattedValue = this.formatter.fromRaw(rawValue, this.model);
+      // @todo strip all tags for formattedValue
+      // @todo Add XSS validation, passing raw data from feed is bad
+      this.$el.append(rawValue);
+      this.delegateEvents();
+      return this;
+    }
+  });
 
-    // Extend cell to include an "html" data type.
-    var DrupalHtmlCell = Backgrid.DrupalHtmlCell = Backgrid.Cell.extend({
-      className: "drupal-html-cell",
-      render: function () {
-        this.$el.empty();
-        var rawValue = this.model.get(this.column.get("name"));
-        // var formattedValue = this.formatter.fromRaw(rawValue, this.model);
-        // @todo strip all tags for formattedValue
-        // @todo Add XSS validation, passing raw data from feed is bad
-        this.$el.append(rawValue);
-        this.delegateEvents();
-        return this;
+  // Extend row to add classes the the row HTML.
+  var DrupalRow = Backgrid.Row.extend({
+
+    render: function () {
+      this.$el.empty();
+
+      var fragment = document.createDocumentFragment();
+      for (var i = 0; i < this.cells.length; i++) {
+        fragment.appendChild(this.cells[i].render().el);
       }
-    });
 
-    $(".backgrid-table", context).each(function(index){
-      var $tableId = this.id;
-      // Retrieve settings from Drupal object.
-      tableSettings = settings.backgrid.tables[$tableId];
+      // Append id.
+      this.el.dataset.id = this.model.attributes.id;
+      // Append type (actually bundle).
+      this.el.dataset.bundle = this.model.attributes.type;
 
-      var bgModel = Backbone.Model.extend({});
+      this.el.appendChild(fragment);
 
-      var bgCollection = Backbone.Collection.extend({
-        model: bgModel,
-        url: tableSettings.dataUrl,
-      });
+      this.delegateEvents();
 
-      var objects = new bgCollection();
+      return this;
+    },
+  });
 
-      // Initialize a new Grid instance
-      var grid = new Backgrid.Grid({
-        columns: tableSettings.columns,
-        collection: objects
-      });
+  Drupal.behaviors.backgrid = {
+    attach: function (context, settings) {
 
-      // Render the grid and attach the root to your HTML document
-      $(this).append(grid.render().el);
+      $(".backgrid-table", context).each(function(index){
+        var $tableId = this.id;
+        // Retrieve settings from Drupal object.
+        tableSettings = settings.backgrid.tables[$tableId];
 
-      // Fetch objects
-      objects.fetch({reset: true});
+        var tableColumns = tableSettings.columns;
 
-    })
+        var bgModel = Backbone.Model.extend({});
 
-  }
-};
+        var bgCollection = Backbone.Collection.extend({
+          model: bgModel,
+          url: tableSettings.dataUrl,
+        });
+
+        var objects = new bgCollection();
+
+        // Initialize a new Grid instance
+        var grid = new Backgrid.Grid({
+          columns: tableSettings.columns,
+          collection: objects,
+          row: DrupalRow,
+        });
+
+        // Render the grid and attach the root to your HTML document
+        $(this).append(grid.render().el);
+
+        // Fetch objects
+        objects.fetch({reset: true});
+
+      })
+
+    }
+  };
 
 })(jQuery);
