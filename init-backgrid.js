9731f6940ac4d942f112087f32b71bd1daaa864d
Wes Jones <waj194@gmail.com>
2015-07-01 10:37:46 -0400

    Add alter hooks and support for 'isDate' flag.

--- init-backgrid.js
+++ init-backgrid.js
@@ -14,7 +14,11 @@
       // var formattedValue = this.formatter.fromRaw(rawValue, this.model);
       // @todo strip all tags for formattedValue
       // @todo Add XSS validation, passing raw data from feed is bad
-      this.$el.append(rawValue);
+      var dateSort = '';
+      if (this.column.get("isDate")) {
+        var dateSort = this.model.get('_' + this.column.get("name"));
+      }
+      this.$el.append(rawValue).data('dateSort', dateSort);
       this.delegateEvents();
       return this;
     }
@@ -46,51 +50,72 @@
 
   Drupal.behaviors.backgrid = {
     attach: function (context, settings) {
-
-      $(".backgrid-table", context).each(function(index){
-        var $tableId = this.id;
+      $(".backgrid-table", context).each(function(index) {
+        var tableId = this.id;
         // Retrieve settings from Drupal object.
-        tableSettings = settings.backgrid.tables[$tableId];
-
-        var tableColumns = tableSettings.columns;
-
-        // For DrupalHtml cell, sort on plain text value.
-        for (id in tableColumns) {
-          if (tableColumns[id].cell == 'drupal-html') {
-            $.extend(tableColumns[id], {
-              sortValue: function (model, sortKey) {
-                //console.log(sortKey);
-                return $(model.get(sortKey)).text().trim();
-              }
-            });
-          }
-        }
-
-        var bgModel = Backbone.Model.extend({});
-
-        var bgCollection = Backbone.Collection.extend({
-          model: bgModel,
-          url: tableSettings.dataUrl,
-        });
-
-        var objects = new bgCollection();
-
-        // Initialize a new Grid instance
-        var grid = new Backgrid.Grid({
-          columns: tableSettings.columns,
-          collection: objects,
-          row: DrupalRow,
-        });
-
-        // Render the grid and attach the root to your HTML document
-        $(this).append(grid.render().el);
-
-        // Fetch objects
-        objects.fetch({reset: true});
-
-      })
-
+        tableSettings = settings.backgrid.tables[tableId];
+        _backgridInit(tableId, tableSettings, context, settings);
+      });
     }
-  };
+  }
+
+  /**
+   * Initialize Backgrid table.
+   */
+  function _backgridInit(tableId, tableSettings, context, settings) {
+    var table = $('#' + tableId, context);
+    var tableColumns = tableSettings.columns;
+
+    // For DrupalHtml cell, sort on plain text value.
+    for (id in tableColumns) {
+      if (tableColumns[id].cell == 'drupal-html') {
+        $.extend(tableColumns[id], {
+          sortValue: function (model, sortKey) {
+            // If there is sort column in the resultset, use that instead.
+            var sortString = '';
+            if (sortString = model.get('_' + sortKey)) {
+              return sortString;
+            }
+            return $(model.get(sortKey)).text().trim();
+          }
+        });
+      }
+    }
+
+    var bgModel = Backbone.Model.extend({});
+    var bgCollection = Backbone.Collection.extend({
+      model: bgModel,
+      url: tableSettings.dataUrl,
+    });
+
+    var objects = new bgCollection();
+
+    // Initialize a new Grid instance
+    var grid = new Backgrid.Grid({
+      columns: tableSettings.columns,
+      collection: objects,
+      row: DrupalRow,
+    });
+
+    // Allow other modules to alter the configuration before rendering.
+    $(document).trigger('backgrid.preRender', [table, tableSettings]);
+
+    // Save the grid object in the Drupal object so we can retrieve it later.
+    Drupal.backgrid = Drupal.backgrid || {};
+    Drupal.backgrid.tables = Drupal.backgrid.tables || [];
+    Drupal.backgrid.tables[tableId] = grid;
+
+    // Fetch objects
+    objects.fetch({reset: true});
+
+    var renderedGrid = grid.render().el;
+
+    // Allow other modules to modify the html of the table.
+    $(document).trigger('backgrid.preAttach', [table, grid, renderedGrid]);
+
+    // Render the grid and attach the root to your HTML document
+    table.append(renderedGrid);
+
+  }
 
 })(jQuery);
